3e4e4cbb678da27044ddd9f8cc0aa290818013ed
BogdanaGonserovska <bogdanag51@gmail.com>
2022-04-16 20:21:28 +0300

    added js

---
+++ JS/login.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+localStorage.removeItem("user");
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    fetch(`http://127.0.0.1:5000/user/login`, {
+        method: "POST",
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then((response) => {
+        if (!response.ok) {
+            document.getElementsByTagName("P")[0].style.visibility = "visible";
+        }
+        return response.json()
+    })
+        .then((data) => {
+            localStorage.setItem("user", JSON.stringify(data));
+            window.open("http://127.0.0.1:5500/HTML/articles.html", "_self");
+        }
+        )
+}
